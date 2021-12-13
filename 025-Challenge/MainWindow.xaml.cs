7901b0ee8f275dee13066c8334a9d733f4e671c1
Abi <oabi2305@gmail.com>
2021-12-13 14:18:37 -0600

    first commit

---
+++ 025-Challenge/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _025_Challenge
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //variables
+
+        string[] values = new string[2];
+
+        //get original number of items
+
+        //get item names and prices in background
+
+        //enter item and price into result
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+
+
+
+        }
+    }
+}
